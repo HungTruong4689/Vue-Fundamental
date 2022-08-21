99eab9d271c6fc34011daafdb85215e1e7792b7c
HungTruong4689 <hungtruong4689@gmail.com>
2022-08-22 00:20:22 +0300

    mapAction vueX

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <base-container title="Vuex">
     <the-counter></the-counter>
+
+    <favorite-value></favorite-value>
     <button @click="addOne">Add 1</button>
+    <change-counter></change-counter>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
 export default {
   components: {
     BaseContainer,
     TheCounter,
+    ChangeCounter,
+    FavoriteValue,
   },
 
   methods: {
     addOne() {
-      this.$store.state.counter++;
+      // this.$store.state.counter++;
+      this.$store.dispatch({
+        type: 'increment',
+      });
     },
   },
 };
---
+++ vuex/src/components/ChangeCounter.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="inc">Add 1</button>
+
+  <button @click="increase({ value: 10 })">Add 10</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  methods: {
+    // addOne() {
+    //   this.$store.commit('increase', { value: 10 });
+    //   this.$store.commit({
+    //     type: 'increase',
+    //     value: 10,
+    //   });
+    //   this.$store.dispatch({
+    //     type: 'increase',
+    //     value: 10,
+    //   });
+    // ...mapActions(['increment', 'increase']),
+    ...mapActions({
+      inc: 'increment',
+      increase: 'increase',
+    }),
+  },
+};
+</script>
