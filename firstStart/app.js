adf2540638d6091d8be21d9f28bf113fdb456194
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-26 10:01:29 +0300

    vue createApp

--- firstStart/app.js
+++ firstStart/app.js
@@ -1,3 +1,21 @@
+Vue.createApp({
+    data: function(){
+        return {
+            goals: [],
+            enteredValue: ''
+        }
+    },
+    methods:{
+        addGoal(){
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+
+}).mount('#app')
+
+
+
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
 // const listEL = document.querySelector('ul');
