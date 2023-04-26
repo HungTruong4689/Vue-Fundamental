f4fec24d2b65ebfc36662ba08e4018e50d15f403
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-26 21:42:13 +0300

    watchers

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -2,16 +2,44 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      fullname: '',
+      lastName: ''
     };
+  },
+  watch: {
+    counter(value){
+      if(value > 50){
+        this.counter = 0;
+      }
+      if(value < 0){
+        this.counter = 0;
+      }
+    }
+    // name(value){
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   }else{
+    //     this.fullname = value + " "+ this.lastName;
+    //   }
+      
+    // },
+    // lastName(value){
+    //   if(value === ''){
+    //     this.fullname = '';
+    //   }else{
+    //     this.fullname = this.name + " "+ value;
+    //   }
+      
+    // }
   },
   computed: {
     fullName(){
       console.log("Changed!!!")
-      if(this.name === ''){
+      if(this.name === '' || this.lastName === ''){
         return '';
       }
-      return this.name + ' '+ 'Mobile';
+      return this.name + ' '+ this.lastName;
     }
   },
 
