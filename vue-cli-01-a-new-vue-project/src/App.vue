df8cf6590eadfd41612ffb513770e54c65bb8514
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-27 18:09:00 +0300

    add new component

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>My Friends</h2>
+  <ul>
+    <li v-for="friend in friends" :key="friend.id">{{ friend.name }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manual@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
