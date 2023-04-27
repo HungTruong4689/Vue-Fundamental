d34931e88de85fe1fc3669767cbfa0c0af5fc01f
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-27 09:56:39 +0300

    adding special attack

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -7,17 +7,37 @@ const app = Vue.createApp({
     data(){
         return{
             playerHealth: 100,
-            monsterHealth:100
+            monsterHealth:100,
+            currentRound: 0
+        }
+    },
+    computed: {
+        monsterBarStyles(){
+            return {width: this.monsterHealth + '%'}
+        },
+        playerBarStyles(){
+            return {width: this.playerHealth + '%'}
+        },
+        mayUseSpecialAttack(){
+            return this.currentRound % 3 !== 0
         }
     },
     methods: {
         attackMonster(){
+            this.currentRound++;
             const attackValue = getRandomValue(5,12);
             this.monsterHealth = this.monsterHealth - attackValue;
+            this.attackPlayer();
         },
         attackPlayer(){
             const attackValue = getRandomValue(8,15);
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster(){
+            this.currentRound++;
+            const attackValue = getRandomValue(10,25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     }
 })
