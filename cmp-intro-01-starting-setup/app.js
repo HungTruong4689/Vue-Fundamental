949f1930ee881ac2d9756334a1a0d6ed24c6d6d1
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-27 16:55:29 +0300

    components intro

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
-        detailsAreVisible: false,
+        
       friends: [
         {
           id: "manuel",
@@ -18,11 +18,34 @@ const app = Vue.createApp({
       ],
     };
   },
-  methods:{
-    toggleDetails(id){
-        this.detailsAreVisible = !this.detailsAreVisible
-    }
-  }
+
 });
+
+app.component('friend-contact', {
+    template: `
+    <li >
+          <h2>{{friend.name}}</h2>
+          <button @click="toggleDetails()">{{ detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone}}</li>
+            <li><strong>Email:</strong> {{ friend.email}}</li>
+          </ul>
+        </li>
+    `,
+    data(){
+        return {detailsAreVisible: false,
+        friend:{
+            id: "manuel",
+            name: "Manuel Lorenz",
+            phone: "01234 5678 991",
+            email: "manuel@localhost.com",
+          }}
+    },
+    methods:{
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible
+        }
+    }
+})
 
 app.mount("#app");
