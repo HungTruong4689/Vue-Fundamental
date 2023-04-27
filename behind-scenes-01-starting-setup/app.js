5c0b3bc096a3d62af7b59f0c2393b0ce080fda18
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-27 12:18:48 +0300

    template

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -16,3 +16,14 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+const app2 = Vue.createApp({
+  template: `<p>{{ favoriteMeal }}</p>`,
+  data() {
+    return {
+      favoriteMeal: 'Pizza'
+    };
+  },
+})
+
+app2.mount('#app2');
