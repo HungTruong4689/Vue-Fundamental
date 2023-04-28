a29a3dbf741aadd13b1cb8cea4b46a15e05ba235
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-28 10:22:19 +0300

    more on slots

--- cmp-adv-01-starting-setup/src/components/BaseCard.vue
+++ cmp-adv-01-starting-setup/src/components/BaseCard.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <header><slot name="header"></slot></header>
+    <header v-if="$slots.header">
+      <slot name="header">
+        <!-- <h2>The Default</h2> -->
+      </slot>
+    </header>
     <slot></slot>
   </div>
 </template>
