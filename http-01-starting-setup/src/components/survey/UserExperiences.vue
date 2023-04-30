dbc5570883bc63370d740c58fd252b8ecbe18c2b
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-30 21:38:32 +0300

    loading page

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -7,7 +7,8 @@
           >Load Submitted Experiences</base-button
         >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -30,10 +31,12 @@ export default {
   data() {
     return {
       results: [],
+      isLoading: false,
     };
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch('https://vue-http-prj-default-rtdb.firebaseio.com/surveys.json', {})
         .then((response) => {
           if (response.ok) {
@@ -41,6 +44,7 @@ export default {
           }
         })
         .then((data) => {
+          this.isLoading = false;
           const results = [];
           for (const id in data) {
             results.push({
