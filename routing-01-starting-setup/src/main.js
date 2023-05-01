d30724a68ffeefa630660733cb7e0ea28648fc0e
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-01 13:50:57 +0300

    named routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -13,10 +13,16 @@ const router = createRouter({
     { path: '/', redirect: '/teams' },
     // { path: '/teams', component: TeamsList, alias: '/' }, //our-domain.com/teams => TeamsList
     {
+      name: 'teams',
       path: '/teams',
       component: TeamsList,
       children: [
-        { path: ':teamId', component: TeamMembers, props: true }, // /teams/t1
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        }, // /teams/t1
       ],
     },
     { path: '/users', component: UsersList },
