3801ae4525f1407364c15e9addb8c1e1ed277c19
HungTruong4689 <hungphuc4689@gmail.com>
2023-05-01 10:35:25 +0300

    navigating router-link

--- routing-01-starting-setup/src/App.vue
+++ routing-01-starting-setup/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <the-navigation @set-page="setActivePage"></the-navigation>
+  <the-navigation></the-navigation>
   <main>
     <router-view></router-view>
   </main>
@@ -18,7 +18,7 @@ export default {
   },
   data() {
     return {
-      activePage: 'teams-list',
+      // activePage: 'teams-list',
       teams: [
         { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
         { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
@@ -40,9 +40,9 @@ export default {
     };
   },
   methods: {
-    setActivePage(page) {
-      this.activePage = page;
-    },
+    // setActivePage(page) {
+    //   this.activePage = page;
+    // },
   },
 };
 </script>
